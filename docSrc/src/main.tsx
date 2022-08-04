9dc16cf10d3e08ba924d65d4347f39e3359c19d6
Mutueye <mutuyueniao@gmail.com>
2022-08-05 00:50:24 +0800

    test windowReady

--- docSrc/src/main.tsx
+++ docSrc/src/main.tsx
@@ -5,4 +5,20 @@ import 'virtual:windi.css';
 // import 'table-flow-graph/dist/index.css';
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(<App />);
+export const domReady = (callBack: () => void) => {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', callBack);
+  } else {
+    callBack();
+  }
+};
+
+export const windowReady = (callBack: () => void) => {
+  if (document.readyState === 'complete') {
+    callBack();
+  } else {
+    window.addEventListener('load', callBack);
+  }
+};
+
+windowReady(() => createRoot(document.getElementById('root')!).render(<App />));
